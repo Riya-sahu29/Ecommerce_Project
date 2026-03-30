5eae7be9d4bbea6b4f7752f59541d75b781bbde6
Riya priyadarsnai sahu <justriya2004@gmail.com>
2026-03-30 17:12:11 +0530

    Fix formatting in ProductCard component

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -9,12 +9,12 @@ function ProductCard({ product }) {
                     src={`${BASEURL}${product.image}`}
                     alt={product.name}
                     className="w-full h-56 object-cover rounded-lg mb-4" />
-                <h2 className="text-lg font-semibold text-gray-800 truncate">{product.name}</h2>
+                <h2 className="text-lg font-semibold text-gray-800 truncate">{product.name}</h2>  
                 <p className="text-gray-800 font-medium">${product.price}</p>
 
             </div>
         </Link>
     )
-}
+}   
 
 export default ProductCard; 
